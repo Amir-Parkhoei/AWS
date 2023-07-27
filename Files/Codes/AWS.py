14b2407d4c0b154069c9e1bad0e13b56a8943213
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-07-28 03:12:24 +0330

    Update files

--- Files/Codes/AWS.py
+++ Files/Codes/AWS.py
@@ -1,5 +1,20 @@
 try:
     from script import Create_Speech
+    try:
+        from tkinter import *
+    except:
+        try:
+            from os import startfile
+            startfile(r"Files\Codes\Download.py")
+        except:
+            try:
+                from time import sleep as sp
+                print("I can't start software.")
+                sp(10)
+                quit()
+            except:
+                print("I can't start software.")
+                quit()
 except:
     try:
         from tkinter import messagebox as message
---
+++ Files/Codes/Download.py
@@ -0,0 +1,5 @@
+from os import system as osy
+print("Download started!")
+osy("pip install tk")
+osy("cls")
+print("Download ended!")
