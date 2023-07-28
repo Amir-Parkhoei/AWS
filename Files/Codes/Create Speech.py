fb43b2349be6677190301e433f80453a8cce88a3
Amir-Parkhoei <140801636+Amir-Parkhoei@users.noreply.github.com>
2023-07-28 12:49:35 +0330

    Update and rename AWS.py to Create Speech.py

--- Files/Codes/Create Speech.py
+++ Files/Codes/Create Speech.py
@@ -1,5 +1,10 @@
 try:
     from script import Create_Speech
+    from script import Get_Text
+    from script import Get_Name
+    text = Get_Text()
+    name = Get_Name()
+    Create_Speech(Text=Def, Speed=125, Name="")
     try:
         from tkinter import *
     except:
