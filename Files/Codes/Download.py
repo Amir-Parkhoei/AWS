52be918e1347fac3888062956da7580c200cc46d
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-07-28 03:43:30 +0330

    UP

--- Files/Codes/Download.py
+++ Files/Codes/Download.py
@@ -4,5 +4,8 @@ with open("Lib-Info.txt", "r") as reader:
     if reader == "Com":
         from script import Check_Internet
         from script import Read_Internet_Info
+        from script import Download_Library
         Check_Internet()
-        
+        Def = Read_Internet_Info()
+        if Def == True: Download_Library()
+        else: print("Connect to internet and try again later."), quit()
